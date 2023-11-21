c869bd55de655f3fb2b6e0f0db073bc012010831
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-11-21 23:37:35 +0100

    All security settings set up for development and functionality added for error messages

--- src/main/java/dk/kea/project/repository/UserRepository.java
+++ src/main/java/dk/kea/project/repository/UserRepository.java
@@ -4,4 +4,5 @@ import dk.kea.project.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    User findByEmail(String email);
 }
